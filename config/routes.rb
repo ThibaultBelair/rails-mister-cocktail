3d83784519a35e2216528f831454860b3fb2fe90
thibault belair <belair.thibault@gmail.com>
2019-08-15 16:05:24 +0200

    ok controllers and routes doses, view todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-resources :cocktails, only: [:index, :create, :new, :show]
-
+  resources :cocktails, only: [:index, :create, :new, :show] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
