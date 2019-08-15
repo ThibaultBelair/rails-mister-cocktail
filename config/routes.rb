c0ded4b19fa94cc141e1766e7501ae3ef7392979
thibault belair <belair.thibault@gmail.com>
2019-08-15 15:14:42 +0200

    ok controller views routes cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
 
+resources :cocktails, only: [:index, :create, :new, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
+# collection do
+#       get 'top' #/cocktails/top
+#     end                                <>>>>>> explication ticket diff member et collection
+#     member do
+#       get 'maker' #/cocktails/:id/maker
+#     end
+#   end
